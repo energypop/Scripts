2ae3e5c89a3659d3f10c5927af833fbc6668429f
Dreampop <sashareconquista@proton.me>
2024-10-19 01:17:17 +0200

    Almost finished script. Uploading to Github

---
+++ ConfigureOhMyPosh.ps1
@@ -0,0 +1,21 @@
+﻿# Installation of omp
+
+winget install JanDeDobbeleer.OhMyPosh -s winget 
+
+if ($?){ # $? is a default variable and it will be used to check if the command has run sucessfully 
+    $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
+    Write-Output "Oh My Posh has been installed"
+    continue 
+}
+else {
+    Write-Output "Couldn't get OhMyPosh from winget. Installing and using scoop instead..."
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    Start-Process scoop -ArgumentList "install git" -Wait 
+    Start-Process scoop -ArgumentList "scoop bucket add extras" -Wait
+    Start-Process scoop -ArgumentList "scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json" -Wait  
+    continue
+}
+
+# Oh my posh is already installed using winget or scoop 
+
